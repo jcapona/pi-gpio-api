a8ad2266c3c27a9f3c2f397352f30445aecb670d
Jorge Capona <jcapona@gmail.com>
2019-08-27 21:57:44 -0400

    Add separate routes for power & io pins

--- pi_gpio_api/core/pi.py
+++ pi_gpio_api/core/pi.py
@@ -26,11 +26,22 @@ class Pi(object):
         GPIO.cleanup()
 
     def __get_function_by_string(self, fcn_string):
+        """
+        Looks the channel_functions dict by key
+        :param fcn_string: string with the value to lookup
+        :return: GPIO library object for the given function
+        """
         for k, v in self.channel_functions.items():
-            if v.lower() == fcn_string:
+            if v.lower() == fcn_string.lower():
                 return k
 
     def set_channel_function(self, channel, fnc):
+        """
+        Sets up a specific channel as input or output
+        :param channel: int with the number of the pin to setup
+        :param fnc: string, either 'input' or 'output'
+        :return: Nothing
+        """
         if channel not in pin_layout.IO_PINS:
             raise AttributeError('Can\'t change function to non IO pins')
 
@@ -38,16 +49,25 @@ class Pi(object):
         GPIO.setup(channel, self.__get_function_by_string(fnc))
 
     def read(self, channel):
-        if channel in pin_layout.POWER_PINS:
-            return
-
+        """
+        Reads the status of a given channel
+        :param channel: int, channel to read
+        :return: value/measure of the given channel
+        """
         try:
             ret = GPIO.input(channel)
         except Exception as e:
             ret = 'ERROR: ' + str(e)
+            print(ret)
         return ret
 
     def write(self, channel, state):
+        """
+        Writes a value into a given pin
+        :param channel: int, channel to change state
+        :param state: boolean, value to write into a pin
+        :return: Nothing
+        """
         if channel not in pin_layout.IO_PINS:
             raise AttributeError('Can\'t write to non IO pins')
 
@@ -58,25 +78,41 @@ class Pi(object):
             print('ERROR: ' + str(e))
 
     def info(self):
-        return {
-            'revision': GPIO.RPI_INFO,
-        }
+        """
+        Returns data specific to the board where the software is running
+        :return: dict with board information
+        """
+        return GPIO.RPI_INFO,
 
     def gpio_status(self, channel=None):
-        pins = pin_layout.ALL_PINS if not channel else [channel]
+        """
+        Creates a dict of data for a given channel/set of channels
+        :param channel: int or list of integers with the channels to fetch
+        data from. If the parameter is not given, returns the status of all
+        channels
+        :return: dict of data abour one/a list of channels
+        """
+        pins = pin_layout.ALL_PINS if not channel else channel
+        if not isinstance(pins, list):
+            pins = [pins]
 
         data = []
         for p in pins:
             status = {}
             status['pin'] = p
-            status['pin_function'] = self.pin_function_name(p)
+            status['pin_function'] = self.__pin_function_name(p)
             if p in pin_layout.IO_PINS:
                 status['pin_state'] = self.read(p)
             data.append(status)
 
         return data
 
-    def pin_function_name(self , pin):
+    def __pin_function_name(self, pin):
+        """
+        Returns the description/use of a given channel
+        :param pin: int, with the number of the pin to lookup
+        :return: string with the description of the pin
+        """
         try:
             descr = self.set_channel_functions[GPIO.gpio_function(pin)]
         except:
